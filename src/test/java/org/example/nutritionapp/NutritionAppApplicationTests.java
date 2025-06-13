b5ce08fdadb3c437d87b7c6d6124939ac9e9b8f5
Your Name <kajiharu0921@gmail.com>
2025-06-13 22:43:52 +0900

    Initial commit

---
+++ src/test/java/org/example/nutritionapp/NutritionAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.nutritionapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutritionAppApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
